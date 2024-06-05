583ca46953a698c5bffc1948a5b2f5f742a571ed
Rambo <ramilsalihar@gmail.com>
2024-06-06 03:19:39 +0600

    notifiers updated

--- lib/providers/event_notifier.dart
+++ lib/providers/event_notifier.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sport_coach/models/event_model.dart';
 import 'package:sport_coach/services/event_service.dart';
 
 class EventNotifier extends ChangeNotifier {
   final EventService _eventService = EventService();
   List<EventModel> _events = [];
+  int _eventIndex = 0;
 
   List<EventModel> get events => _events;
 
   bool get isEmpty => _events.isEmpty;
+
+  int get eventIndex => _eventIndex;
+
+  EventNotifier() {
+    _loadEventIndex();
+  }
+
+  Future<void> _loadEventIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    _eventIndex = prefs.getInt('eventIndex') ?? 0;
+    notifyListeners();
+  }
+
+  Future<int> getEventIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    _eventIndex = prefs.getInt('eventIndex') ?? 0;
+    return _eventIndex;
+  }
+
+  Future<void> setEventIndex(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('eventIndex', index);
+    _eventIndex = index;
+    notifyListeners();
+  }
 
   Future<void> loadEvents() async {
     _events = await _eventService.getEvents();
@@ -17,6 +44,7 @@ class EventNotifier extends ChangeNotifier {
 
   Future<void> addEvent(EventModel event) async {
     await _eventService.addEvent(event);
+    await setEventIndex(_eventIndex + 1);
     await loadEvents();
   }
 
@@ -27,6 +55,15 @@ class EventNotifier extends ChangeNotifier {
 
   Future<void> deleteEvent(int index) async {
     await _eventService.deleteEvent(index);
+    await setEventIndex(_eventIndex - 1);
     await loadEvents();
+  }
+
+  EventModel? findByIndex(int index) {
+    try {
+      return _events.firstWhere((event) => event.index == index);
+    } catch (e) {
+      return null;
+    }
   }
 }
--- lib/providers/training_notifier.dart
+++ lib/providers/training_notifier.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sport_coach/models/training_model.dart';
 import 'package:sport_coach/services/training_service.dart';
 
 class TrainingNotifier extends ChangeNotifier {
   final TrainingService _trainingService = TrainingService();
   List<TrainingModel> _trainings = [];
+  int _trainingIndex = 0;
 
   List<TrainingModel> get trainings => _trainings;
 
   bool get isEmpty => _trainings.isEmpty;
+
+  int get trainingIndex => _trainingIndex;
+
+  TrainingNotifier() {
+    _loadTrainingIndex();
+  }
+
+  Future<void> _loadTrainingIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    _trainingIndex = prefs.getInt('trainingIndex') ?? 0;
+    notifyListeners();
+  }
+
+  Future<int> getTrainingIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    _trainingIndex = prefs.getInt('trainingIndex') ?? 0;
+    return _trainingIndex;
+  }
+
+  Future<void> setTrainingIndex(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('trainingIndex', index);
+    _trainingIndex = index;
+    notifyListeners();
+  }
 
   Future<void> loadTrainings() async {
     _trainings = await _trainingService.getTrainings();
@@ -17,6 +44,7 @@ class TrainingNotifier extends ChangeNotifier {
 
   Future<void> addTraining(TrainingModel training) async {
     await _trainingService.addTraining(training);
+    await setTrainingIndex(_trainingIndex + 1);
     await loadTrainings();
   }
 
@@ -27,6 +55,15 @@ class TrainingNotifier extends ChangeNotifier {
 
   Future<void> deleteTraining(int index) async {
     await _trainingService.deleteTraining(index);
+    await setTrainingIndex(_trainingIndex - 1);
     await loadTrainings();
+  }
+
+  TrainingModel? findByIndex(int index) {
+    try {
+      return _trainings.firstWhere((training) => training.index == index);
+    } catch (e) {
+      return null;
+    }
   }
 }
